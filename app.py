89d0983a7f8882454d6422a67f889ac5d7ba157a
SiddharthKathairia <siddharthkathairia@gmail.com>
2026-03-25 19:32:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import fitz  # PyMuPDF
+import pymupdf as fitz
 import google.generativeai as genai
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from sentence_transformers import SentenceTransformer
